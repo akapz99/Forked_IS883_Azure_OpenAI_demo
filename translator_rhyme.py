0574e07924a193fbda976e0fdec2558233fdd15c
akapz99 <122059558+akapz99@users.noreply.github.com>
2023-12-09 14:16:43 -0500

    Update translator_rhyme.py

--- translator_rhyme.py
+++ translator_rhyme.py
@@ -1,39 +1,21 @@
 import streamlit as st
-import openai
 import os
-from deep_translator import GoogleTranslator
+import openai
 
 # Set up the OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Function to generate lyrics
-def generate_lyrics(artist_name, genre, subject=None, rhyme=None, temperature=0.7, use_slang=False):
-    prompt = f"Imagine you are a songwriter. Write the lyrics to a song based on this {genre} that the author wants: {subject}, in similarity to the style of this artist: {artist_name}, and if available create rhymes with this phrase {rhyme}. Try your best to match the style of the artist. Unless specified, do not use slang or casual language in the lyrics generated. Unless specified, do not translate the lyrics. If translation is specified, try to ensure that the translation rhymes."
+# ... (unchanged)
 
-    # Modify the prompt based on the use_slang parameter
-    if use_slang:
-        prompt += " You are allowed to use slang and casual language in the lyrics in this case."
-
-    # Generate lyrics using OpenAI GPT-3
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=200,
-        temperature=temperature,
-    )
-
-    generated_lyric = response['choices'][0]['text']
-    return generated_lyric
-
-# Function to translate text to Hindi and add rhyming translations
-def translate_to_hindi_with_rhyme(text, rhyme, temperature=0.7):
-    # Add rhyming component to the translation prompt
-    prompt = f"Translate the following lines into rhyming Hindi:\n"
-    prompt += f'"{text}"'
+# Function to translate text to the selected language and add rhyming translations
+def translate_and_rhyme(text, language, rhyme, temperature=0.7):
+    translation_prompt = f"Translate the following lines into rhyming {language}:\n"
+    translation_prompt += f'"{text}"'
 
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=prompt,
+        prompt=translation_prompt,
         max_tokens=200,
         temperature=temperature,
     )
@@ -51,7 +33,8 @@ subject = st.text_input("Subject (Optional):", "Enter the subject for this parti
 rhyme = st.text_input("Rhyme (Optional):", "Enter a particular word or phrase that you would like used")
 temperature = st.slider("Select how much personal (random) input you would like from me (the AI)", 0.1, 1.0, 0.7, 0.1)
 use_slang = st.checkbox("Allow Slang in Lyrics", value=False, key='slang_checkbox', help='Use slang and casual language in the lyrics.')
-translate_hindi = st.checkbox("Translate to Hindi", value=False, help="Check this box if you want to translate the lyrics to Hindi.")
+selected_language = st.selectbox("Select Language", ["English", "Hindi", "French", "Arabic", "German", "Italian", "Spanish"])
+translate_selected_language = st.checkbox(f"Translate to {selected_language}", value=False, help=f"Check this box if you want to translate the lyrics to {selected_language}.")
 
 # Generate lyrics when the user clicks the button
 if st.button("Generate Lyrics"):
@@ -62,10 +45,10 @@ if st.button("Generate Lyrics"):
         # Display the generated lyric
         st.success(f"Generated Lyric:\n{generated_lyric}")
 
-        # Translate to Hindi with rhyming if requested
-        if translate_hindi:
-            translated_lyric = translate_to_hindi_with_rhyme(generated_lyric, rhyme, temperature)
-            st.success(f"Translated to Hindi with Rhyme:\n{translated_lyric}")
+        # Translate to the selected language with rhyming if requested
+        if translate_selected_language:
+            translated_lyric = translate_and_rhyme(generated_lyric, selected_language.lower(), rhyme, temperature)
+            st.success(f"Translated to {selected_language} with Rhyme:\n{translated_lyric}")
 
         # Ask for user feedback
         user_feedback = st.selectbox("How satisfied are you with the generated lyric?", ["Satisfied", "Neutral", "Dissatisfied"])
@@ -73,3 +56,4 @@ if st.button("Generate Lyrics"):
         # Use user feedback to refine the model if dissatisfied
         if user_feedback == "Dissatisfied":
             st.info("Thank you for your feedback! We will use this to improve our lyric generation.")
+
